af2bc7252a0d89cb7b589c64884737e1f1f3b683
Greg Smith <smr.kyu@gmail.com>
2013-04-11 02:21:29 -0500

    threaded renderer to help with responsiveness; wildcards in directory search

--- rapidshow.py
+++ rapidshow.py
@@ -1,45 +1,56 @@
 #/usr/bin/env python
 
-import os, argparse, random, threading, pygame
+import os, argparse, glob, random, threading, pygame
 
 def is_image(filename):
 	filename = filename.lower()
 	return filename[filename.rfind(".")+1:] in ["jpg", "jpeg", "png", "gif"]
 
-def pick_random_image(dirs):
-	images = [os.path.join(dir, f) for dir in dirs for f in os.listdir(dir) if is_image(f)]
+def get_images_from_glob(pathname):
+	return [f for f in glob.glob(pathname) if is_image(f)]
+
+def get_images_from_dir(pathname):
+	return [os.path.join(pathname, f) for f in os.listdir(pathname) if is_image(f)]
+
+def get_image_from_filename(pathname):
+	if is_image(pathname):
+		return [pathname]
+	return []
+
+def pick_random_image(pathnames):
+	images = []
+	for pathname in pathnames:
+		if '*' in pathname or '?' in pathname:
+			images.extend(get_images_from_glob(pathname))
+		elif os.path.isdir(pathname):
+			images.extend(get_images_from_dir(pathname))
+		elif os.path.isfile(pathname):
+			images.extend(get_image_from_filename(pathname))
 	return random.choice(images)
 
-def get_blit_position(surface, screen):
-	surfaceWidth = surface.get_width()
-	surfaceHeight = surface.get_height()
-	screenWidth = screen.get_width()
-	screenHeight= screen.get_height()
-	return ((screenWidth - surfaceWidth)/2, (screenHeight - surfaceHeight)/2)
-
-class Slides:
+class SlideLoader:
 	buffer = []
-	bufferLength = 0
+	bufferSize = 0
 	lock = threading.Lock()
 	loader = None
 
-	def __init__(self, dirs, bufferLength):
-		self.loader = InfiniteLoopThread(lambda: self.load_random_slide(dirs))
-		self.bufferLength = bufferLength
+	def __init__(self, pathnames, bufferSize):
+		self.loader = InfiniteLoopThread(lambda: self.load_random_slide(pathnames))
+		self.bufferSize = bufferSize
 
 	def start_loading(self):
 		self.loader.start()
 
-	def is_buffer_primed(self):
-		return len(self.buffer) > min(3, self.bufferLength)
+	def get_buffer_amount(self):
+		return len(self.buffer) / self.bufferSize
 
-	def load_random_slide(self, dirs):
-		filename = pick_random_image(dirs)
-		slide = pygame.image.load(filename).convert()
+	def load_random_slide(self, pathnames):
+		filename = pick_random_image(pathnames)
+		slide = pygame.image.load(open(filename, "rb")).convert()
 
 		self.lock.acquire()
 		self.buffer.append(slide)
-		if len(self.buffer) > self.bufferLength:
+		if len(self.buffer) > self.bufferSize:
 			self.buffer.pop(0)
 		self.lock.release()
 
@@ -59,39 +70,78 @@ class InfiniteLoopThread(threading.Thread):
 		while 1:
 			self.fn()
 
-def run(dirs, fps, backgroundColor, bufferLength):
-	pygame.init()
-	screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN | pygame.DOUBLEBUF | pygame.HWSURFACE)
-	pygame.mouse.set_visible(0)
-	clock = pygame.time.Clock()
+def get_blit_position(surface, screen):
+	surfaceWidth = surface.get_width()
+	surfaceHeight = surface.get_height()
+	screenWidth = screen.get_width()
+	screenHeight= screen.get_height()
+	return ((screenWidth - surfaceWidth)/2, (screenHeight - surfaceHeight)/2)
 
-	slides = Slides(dirs, bufferLength)
-	slides.start_loading()
-	while not slides.is_buffer_primed():
-		clock.tick(20)
+class Renderer(threading.Thread):
+	daemon = True
+	screen = None
+	slideLoader = None
+	fps = None
+	minimumBufferFill = None
 
-	while 1:
-		clock.tick(fps)
-		slide = slides.pick_random_slide()
-		screen.fill(backgroundColor)
-		screen.blit(slide, get_blit_position(slide, screen))
-		pygame.display.flip()
+	def __init__(self, screen, slideLoader, fps, minimumBufferFill, showFps):
+		threading.Thread.__init__(self)
+		self.screen = screen
+		self.slideLoader = slideLoader
+		self.fps = fps
+		self.minimumBufferFill = minimumBufferFill
+		self.showFps = showFps
 
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				return
-			elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-				return
+	def run(self):
+		font = pygame.font.Font(None, 48)
+		clock = pygame.time.Clock()
+		backgroundColor = (0, 0, 0)
+		textColor = (30, 30, 50)
+
+		while 1:
+			clock.tick(self.fps)
+			self.screen.fill(backgroundColor)
+			bufferAmount = self.slideLoader.get_buffer_amount()
+			if bufferAmount < self.minimumBufferFill:
+				text = 'Buffering... ({:.1%})'.format(bufferAmount / self.minimumBufferFill)
+				textSurface = font.render(text, 1, textColor)
+				self.screen.blit(textSurface, get_blit_position(textSurface, self.screen))
+			else:
+				slide = self.slideLoader.pick_random_slide()
+				self.screen.fill(backgroundColor)
+				self.screen.blit(slide, get_blit_position(slide, self.screen))
+			if self.showFps:
+				text = 'fps: {:.1f}'.format(clock.get_fps())
+				textSurface = font.render(text, 1, textColor)
+				self.screen.blit(textSurface, (10, 10))
+			pygame.display.flip()
 
 def main():
 	parser = argparse.ArgumentParser(description='Display slides. Fast.')
 	parser.add_argument('dir', help='Read images from DIR', nargs='+')
 	parser.add_argument('-f', '--fps', help='Sets the framerate for displaying new slides', metavar='##', type=int, default=60)
 	parser.add_argument('--buf', dest='bufferSize', help='Sets the size of the slide buffer', type=int, default=100, metavar='##')
-
+	parser.add_argument('--fill', dest='minimumBufferFill', help='Sets the minimum buffer fill ratio before displaying begins', type=float, default=0.25, metavar='0.##')
+	parser.add_argument('--debug', action='store_true')
 	options = parser.parse_args()
 
-	backgroundColor = (0, 0, 0)
-	run(options.dir, options.fps, backgroundColor, options.bufferSize)
+	pygame.init()
+	screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN | pygame.DOUBLEBUF | pygame.HWSURFACE)
+	pygame.mouse.set_visible(0)
+
+	slideLoader = SlideLoader(options.dir, options.bufferSize)
+	slideLoader.start_loading()
+
+	renderer = Renderer(screen, slideLoader, options.fps, options.minimumBufferFill, options.debug)
+	renderer.start()
+
+	clock = pygame.time.Clock()
+	while 1:
+		clock.tick(5)
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+			elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+				return
 
 if __name__ == '__main__': main()
